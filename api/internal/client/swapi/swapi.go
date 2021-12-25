539763a6a126ee4a4a8aca51163bbc2d1b066686
broozkan <burhan.ozkan@modanisa.com>
2021-12-25 16:31:50 +0300

    Created swapi client implementations

---
+++ api/internal/client/swapi/swapi.go
@@ -0,0 +1,35 @@
+package swapi
+
+import (
+	"crypto/tls"
+	"net/http"
+	"time"
+)
+
+type Swapi struct {
+	URL  string
+	http *http.Client
+}
+
+func NewSwapiClient(url string) *Swapi {
+
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: false,
+			MinVersion:         tls.VersionTLS12,
+		},
+	}
+	client := http.Client{
+		Timeout:   30 * time.Second,
+		Transport: transport,
+	}
+
+	return &Swapi{
+		URL:  url,
+		http: &client,
+	}
+}
+
+func (s *Swapi) GetStarshipDetails(starshipName string) (string, error) {
+	panic("implement me")
+}
